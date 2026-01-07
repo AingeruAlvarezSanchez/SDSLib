ab199604b64ed50ad8b30a7cea3ef060a371eb6f
aalvarez <aingeru.develop@gmail.com>
2026-01-07 20:30:15 +0100

    feat(SDSLib): add initial project setup with MonoGame framework
    - `SDSLib.csproj` configured for .NET 9.0 and MonoGame.Framework.DesktopGL package.
    - `SDSLib.cs` with a basic Game class implementation as a starting point.
    - `.gitignore` for build results and configuration files.
    
    Signed-off-by: aalvarez <aingeru.develop@gmail.com>

---
+++ SDSLib.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SDSLib;
+
+public class SdsLib : Game {
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    protected SdsLib(string gameTitle, string contentRoot) {
+        _graphics = new GraphicsDeviceManager(this);
+        Window.Title = gameTitle;
+        Content.RootDirectory = contentRoot;
+        IsMouseVisible = true;
+        SdsInstance = this;
+    }
+
+    public static SdsLib SdsInstance { get; private set; }
+
+    protected override void Initialize() {
+        base.Initialize();
+    }
+
+    protected override void LoadContent() {
+        base.LoadContent();
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime) {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime) {
+        GraphicsDevice.Clear(Color.Black);
+        base.Draw(gameTime);
+    }
+}
